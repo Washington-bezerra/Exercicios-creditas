fef48861a0f9fa5557c41f0b01b113f4c48622b4
washington-bezerra <luiswashington00@gmail.com>
2022-02-25 14:00:33 -0300

    Refatoramento

--- Exercicios-creditas/Jan-ken-po-Torneio/Jan-Ken-Po_chaves.js
+++ Exercicios-creditas/Jan-ken-po-Torneio/Jan-Ken-Po_chaves.js
@@ -8,24 +8,69 @@ let array =
     [   
         [      ['Allen', 'TE'], ['Arthur', 'TE']    ],
         [      ['David', 'PA'], ['Omer', 'PE']    ],
-    ],
-
-    [   
-        [      ['Paulo', 'TE'], ['José', 'TE']    ],
-        [      ['Bruno', 'PA'], ['Thiago', 'PE']    ],
-    ],
-
-    [   
-        [      ['Caio', 'PE'], ['Geovanna', 'PE']    ],
-        [      ['Ana', 'PA'], ['Priscila', 'PE']    ],
-    ],
+    ]
 ];
 
-arrayGanhadores = []
+function batalha(escolhaJogador1, escolhaJogador2, array, arrayGanhadores, cont, contador, conta){
+    
+    if (cont != null){
+        if (escolhaJogador1 == escolhaJogador2){
 
-function removePerdedor(array, cont, arrayGanhadores, matamata, fim){
+            console.log(`Empate! O jogador >${array[cont][contador][0]}< ganhou do ${array[cont][contador][1]}`);
+            arrayGanhadores.push([array[cont][contador][0][0], array[cont][contador][0][1]]);
+        }
+
+        else if (escolhaJogador1 == 'PE' & escolhaJogador2 == 'TE'){
+            console.log(`O jogador >${array[cont][contador][0]}< ganhou do ${array[cont][contador][1]}`);
+            arrayGanhadores.push([array[cont][contador][0][0], array[cont][contador][0][1]]);
+        }
+
+        else if (escolhaJogador1 == 'TE' & escolhaJogador2 == 'PA'){
+            console.log(`O jogador >${array[cont][contador][0]}< ganhou do ${array[cont][contador][1]}`);
+            arrayGanhadores.push([array[cont][contador][0][0], array[cont][contador][0][1]]);
+        }
+        
+        else if (escolhaJogador1 == 'PA' & escolhaJogador2 == 'PE'){
+            console.log(`O jogador >${array[cont][contador][0]}< ganhou do ${array[cont][contador][1]}`);
+            arrayGanhadores.push([array[cont][contador][0][0], array[cont][contador][0][1]]);
+        }
+        else{
+            console.log(`O jogador >${array[cont][contador][1]}< ganhou do ${array[cont][contador][0]}`);
+            arrayGanhadores.push([array[cont][contador][1][0], array[cont][contador][1][1]]);
+        }
+    }else{
+
+        if (escolhaJogador1 == escolhaJogador2){
+            console.log(`Empate! O jogador >${arrayGanhadores[conta]}< ganhou do ${arrayGanhadores[conta+1]}`);
+            arrayGanhadores[conta+1] = null;
+        }
+    
+        else if (escolhaJogador1 == 'PE' & escolhaJogador2 == 'TE'){
+            console.log(`O jogador >${arrayGanhadores[conta]}< ganhou do ${arrayGanhadores[conta+1]}`);
+            arrayGanhadores[conta+1] = null;
+        }   
+        else if (escolhaJogador1 == 'TE' & escolhaJogador2 == 'PA'){
+            console.log(`O jogador >${arrayGanhadores[conta]}< ganhou do ${arrayGanhadores[conta+1]}`);
+            arrayGanhadores[conta+1] = null;
+        }       
+        else if (escolhaJogador1 == 'PA' & escolhaJogador2 == 'PE'){
+            console.log(`O jogador >${arrayGanhadores[conta]}< ganhou do ${arrayGanhadores[conta+1]}`);
+            arrayGanhadores[conta+1] = null;
+        }
+        else{
+            console.log(`O jogador >${arrayGanhadores[conta+1]}< ganhou do ${arrayGanhadores[conta]}`);
+            arrayGanhadores[conta] = null
+        }
+    }
+}
+
+arrayGanhadores = []
+var fim = false
+
+function removePerdedor(array, cont, arrayGanhadores, matamata){
 
     if(!matamata){
+
         if (cont < array.length){
             var contador = 0;
 
@@ -34,123 +79,43 @@ function removePerdedor(array, cont, arrayGanhadores, matamata, fim){
                 var escolhaJogador1 = array[cont][contador][0][1];
                 var escolhaJogador2 = array[cont][contador][1][1];
 
-                if (escolhaJogador1 == escolhaJogador2){
-
-                    console.log(`Empate! O jogador >${array[cont][contador][0]}< ganhou do ${array[cont][contador][1]}`);
-                    arrayGanhadores.push([array[cont][contador][0][0], array[cont][contador][0][1]]);
-                }
-            
-                else if (escolhaJogador1 == 'PE' & escolhaJogador2 == 'TE'){
-                    console.log(`O jogador >${array[cont][contador][0]}< ganhou do ${array[cont][contador][1]}`);
-                    arrayGanhadores.push([array[cont][contador][0][0], array[cont][contador][0][1]]);
-                }
-                else if (escolhaJogador1 == 'PE' & escolhaJogador2 != 'TE'){
-                    console.log(`O jogador >${array[cont][contador][1]}< ganhou do ${array[cont][contador][0]}`);
-                    arrayGanhadores.push([array[cont][contador][1][0], array[cont][contador][1][1]]);
-                }
-            
-                else if (escolhaJogador1 == 'TE' & escolhaJogador2 == 'PA'){
-                    console.log(`O jogador >${array[cont][contador][0]}< ganhou do ${array[cont][contador][1]}`);
-                    arrayGanhadores.push([array[cont][contador][0][0], array[cont][contador][0][1]]);
-                }
-                else if (escolhaJogador1 == 'TE' & escolhaJogador2 != 'PA'){
-                    console.log(`O jogador >${array[cont][contador][1]}< ganhou do ${array[cont][contador][0]}`);
-                    arrayGanhadores.push([array[cont][contador][1][0], array[cont][contador][1][1]]);
-                }
-                
-                else if (escolhaJogador1 == 'PA' & escolhaJogador2 == 'PE'){
-                    console.log(`O jogador >${array[cont][contador][0]}< ganhou do ${array[cont][contador][1]}`);
-                    arrayGanhadores.push([array[cont][contador][0][0], array[cont][contador][0][1]]);
-                }
-                else if (escolhaJogador1 == 'PA' & escolhaJogador2 != 'PE'){
-                    console.log(`O jogador >${array[cont][contador][1]}< ganhou do ${array[cont][contador][0]}`);
-                    arrayGanhadores.push([array[cont][contador][1][0], array[cont][contador][1][1]]);
-                }
+                batalha(escolhaJogador1, escolhaJogador2, array, arrayGanhadores, cont, contador);
 
                 contador += 1;
-        
             }
 
         }else{
             if (cont >= array.length){
-                console.log(arrayGanhadores)
                 removePerdedor(array, cont, arrayGanhadores, true)
             }else{
                 console.log(arrayGanhadores);
             }
         }
+
     }else{
         let conta = 0;
-        var arrayGanhadores = arrayGanhadores.filter((element) => element != null);
-
-        if(arrayGanhadores.length == 1){
-            return console.log(`O ganhador é: ${arrayGanhadores}`);
-        }
         
         while (conta < (arrayGanhadores.length-1)){
 
             var escolhaJogador1 = arrayGanhadores[conta][1];
             var escolhaJogador2 = arrayGanhadores[conta+1][1];
 
-
-            if (escolhaJogador1 == escolhaJogador2){
-                //console.log(`>>>>>>>>${escolhaJogador1}`)
-                console.log(`Empate! O jogador >${arrayGanhadores[conta]}< ganhou do ${arrayGanhadores[conta+1]}`);
-                //arrayGanhadores.splice((conta+1), 1);
-                arrayGanhadores[conta+1] = null;
-            }
-        
-            else if (escolhaJogador1 == 'PE' & escolhaJogador2 == 'TE'){
-                console.log(`O jogador >${arrayGanhadores[conta]}< ganhou do ${arrayGanhadores[conta+1]}`);
-                //arrayGanhadores.splice((conta+1), 1);
-                arrayGanhadores[conta+1] = null;
-            }
-            else if (escolhaJogador1 == 'PE' & escolhaJogador2 != 'TE'){
-                console.log(`O jogador >${arrayGanhadores[conta+1]}< ganhou do ${arrayGanhadores[conta]}`);
-                //arrayGanhadores.splice(conta, 1);
-                arrayGanhadores[conta] = null
-            }
-        
-            else if (escolhaJogador1 == 'TE' & escolhaJogador2 == 'PA'){
-                console.log(`O jogador >${arrayGanhadores[conta]}< ganhou do ${arrayGanhadores[conta+1]}`);
-                //arrayGanhadores.splice((conta+1), 1);
-                arrayGanhadores[conta+1] = null;
-            }
-            else if (escolhaJogador1 == 'TE' & escolhaJogador2 != 'PA'){
-                console.log(`O jogador >${arrayGanhadores[conta+1]}< ganhou do ${arrayGanhadores[conta]}`);
-                //arrayGanhadores.splice(conta, 1);
-                arrayGanhadores[conta] = null
-            }
-            
-            else if (escolhaJogador1 == 'PA' & escolhaJogador2 == 'PE'){
-                console.log(`O jogador >${arrayGanhadores[conta]}< ganhou do ${arrayGanhadores[conta+1]}`);
-                //arrayGanhadores.splice((conta+1), 1);
-                arrayGanhadores[conta+1] = null;
-            }
-            else if (escolhaJogador1 == 'PA' & escolhaJogador2 != 'PE'){
-                console.log(`O jogador >${arrayGanhadores[conta+1]}< ganhou do ${arrayGanhadores[conta]}`);
-                //arrayGanhadores.splice(conta, 1);
-                arrayGanhadores[conta] = null
-            }
-
-            console.log(arrayGanhadores)
+            batalha(escolhaJogador1, escolhaJogador2, array, arrayGanhadores, null, null, conta)
 
             conta+=2
         }
+
+        var arrayGanhadores = arrayGanhadores.filter((element) => element != null);
+        console.log(arrayGanhadores)
+
+        if(arrayGanhadores.length == 1){
+            fim = true;
+            return console.log(`O ganhador é: ${arrayGanhadores}`);
+        }
     }
-
-    removePerdedor(array, cont+=1, arrayGanhadores, false)
-}
-
-//removePerdedor(array, 0, arrayGanhadores, false)
-
-
-function teste (cont){
-    console.log(cont)
-    if (cont > 10){
-        return console.log("fim!!")
+    if (!fim){
+        removePerdedor(array, cont+=1, arrayGanhadores, false)
     }
-    teste(cont+1)
-}
+}   
 
-teste(1)
+removePerdedor(array, 0, arrayGanhadores, false)
--- Exercicios-creditas/Jan-ken-po/Jan-Ken-Po.js
+++ Exercicios-creditas/Jan-ken-po/Jan-Ken-Po.js
@@ -1,3 +1,25 @@
+function batalha(escolhaJogador1, escolhaJogador2, game){
+
+    if (escolhaJogador1 == escolhaJogador2){
+        return game[0];
+    }
+
+    else if (escolhaJogador1 == 'PE' & escolhaJogador2 == 'TE'){
+        return game[0];
+    }
+    
+    else if (escolhaJogador1 == 'TE' & escolhaJogador2 == 'PA'){
+        return game[0];
+    }
+    
+    else if (escolhaJogador1 == 'PA' & escolhaJogador2 == 'PE'){
+        return game[0];
+    }
+    else{
+        return game[1]
+    }
+}
+
 function rpsGameWinner(game) {
     if (game.length != 2){
         throw new Error('WrongNumberOfPlayers');
@@ -11,30 +33,7 @@ function rpsGameWinner(game) {
     let escolhaJogador1 = game[0][1].toUpperCase();
     let escolhaJogador2 = game[1][1].toUpperCase();
 
-    if (escolhaJogador1 == escolhaJogador2){
-        return game[0];
-    }
-
-    if (escolhaJogador1 == 'PE' & escolhaJogador2 == 'TE'){
-        return game[0];
-    }
-    if (escolhaJogador1 == 'PE' & escolhaJogador2 != 'TE'){
-        return game[1];
-    }
-
-    if (escolhaJogador1 == 'TE' & escolhaJogador2 == 'PA'){
-        return game[0];
-    }
-    if (escolhaJogador1 == 'TE' & escolhaJogador2 != 'PA'){
-        return game[1];
-    }
-    
-    if (escolhaJogador1 == 'PA' & escolhaJogador2 == 'PE'){
-        return game[0];
-    }
-    if (escolhaJogador1 == 'PA' & escolhaJogador2 != 'PE'){
-        return game[1];
-    }
+    console.log(batalha(escolhaJogador1, escolhaJogador2, game))
 }
 
-console.log(rpsGameWinner([['Eu', 'pe']]))
+rpsGameWinner([['Eua', 'pa'], ['Eu', 'pe']])
